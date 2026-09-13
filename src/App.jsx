8a13572e489e791fc486de7f0571b01e77d08058
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2026-09-13 20:18:56 -0300

    feat(header): incorporar navegacion principal y menu responsive. Se maqueto el componente Header con navegacion adaptable y menu hamburguesa para pantallas pequenas. Provee la estructura visual base dentro del MainLayout para la navegacion de la app.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import { MainLayout } from './layout/MainLayout/MainLayout'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
